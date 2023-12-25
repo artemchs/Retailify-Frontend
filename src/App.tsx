94a35cca7c933ff4eb6e28abc6e4c3d657fad3b0
cherartem <artem138927@gmail.com>
2023-12-25 17:39:10 +0100

    build: set up @tanstack/react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-const App = () => {
-  return <>Hello World!</>
-}
+import { Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
-export default App
+export default function App() {
+  return (
+    <div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import {
+  RouterProvider,
+  Router,
+  Route,
+  RootRoute,
+} from '@tanstack/react-router'
+
+const rootRoute = new RootRoute({
+  component: () => <App />,
+})
+
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: function Index() {
+    return (
+      <div className='p-2'>
+        <h3>Welcome Home!</h3>
+      </div>
+    )
+  },
+})
+
+const routeTree = rootRoute.addChildren([indexRoute])
+
+const router = new Router({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
