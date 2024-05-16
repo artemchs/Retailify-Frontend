5431983a759bcc341eac07ac90d2c8776cd5ab2d
Artem Chernysh <artem_chernysh@outlook.com>
2024-05-16 19:13:00 +0200

    feat(goods-receipts): :sparkles: display additional info in goods receipts table

--- src/features/goods-receipts/components/table/columns.tsx
+++ src/features/goods-receipts/components/table/columns.tsx
@@ -1,76 +1,111 @@
 import SortableDataTableHeader from '@/components/ui/sortable-data-table-header'
 import { CurrencyFormatter, DateFormatter } from '@/components/ui/units'
-import {
-  GoodsReceipt,
-  paymentOptions,
-} from '@/types/entities/GoodsReceipt'
+import { GoodsReceipt, paymentOptions } from '@/types/entities/GoodsReceipt'
 import { ColumnDef } from '@tanstack/react-table'
 import GoodsReceiptActions from './GoodsReceiptActions'
 
 export const columns: ColumnDef<GoodsReceipt>[] = [
-  {
-    id: 'Название',
-    accessorKey: 'name',
-    header: () => (
-      <SortableDataTableHeader
-        label='Название'
-        orderByProperty='name'
-        routeId='/layout/goods-receipts'
-      />
-    ),
-    cell: ({ row }) => <span className='font-medium'>{row.original.name}</span>,
-  },
-  {
-    id: 'Дата прихода',
-    accessorKey: 'goodsReceiptDate',
-    header: () => (
-      <SortableDataTableHeader
-        label='Дата прихода'
-        orderByProperty='goodsReceiptDate'
-        routeId='/layout/goods-receipts'
-      />
-    ),
-    cell: ({ row }) => <DateFormatter date={row.original.goodsReceiptDate} />,
-  },
-  {
-    id: 'Склад',
-    header: 'Склад',
-    cell: ({ row }) => row.original.warehouse?.name,
-  },
-  {
-    id: 'Поставщик',
-    header: 'Поставщик',
-    cell: ({ row }) => row.original.supplier?.name,
-  },
-  {
-    id: 'Способ оплаты',
-    accessorFn: (row) =>
-      row.supplierInvoice?.paymentOption &&
-      paymentOptions[row.supplierInvoice?.paymentOption],
-  },
-  {
-    id: 'Общая стоимость',
-    header: () => <div className='text-right'>Общая стоимость</div>,
-    cell: ({ row }) => {
-      const totalCost = row.original.supplierInvoice?.accountsPayable
-        ? parseFloat(row.original.supplierInvoice?.accountsPayable)
-        : 0
-
-      return (
-        <div className='w-full text-right'>
-          <CurrencyFormatter value={totalCost} />
-        </div>
-      )
+    {
+        id: 'Название',
+        accessorKey: 'name',
+        header: () => (
+            <SortableDataTableHeader
+                label='Название'
+                orderByProperty='name'
+                routeId='/layout/goods-receipts'
+            />
+        ),
+        cell: ({ row }) => (
+            <span className='font-medium'>{row.original.name}</span>
+        ),
     },
-  },
-  {
-    id: 'actions',
-    cell: ({ row }) => {
-      const { id, isArchived } = row.original
-
-      return <GoodsReceiptActions id={id} isArchived={isArchived} />
+    {
+        id: 'Дата прихода',
+        accessorKey: 'goodsReceiptDate',
+        header: () => (
+            <SortableDataTableHeader
+                label='Дата прихода'
+                orderByProperty='goodsReceiptDate'
+                routeId='/layout/goods-receipts'
+            />
+        ),
+        cell: ({ row }) => (
+            <DateFormatter date={row.original.goodsReceiptDate} />
+        ),
     },
-    enableSorting: false,
-    enableHiding: false,
-  },
+    {
+        id: 'Склад',
+        header: 'Склад',
+        cell: ({ row }) => row.original.warehouse?.name,
+    },
+    {
+        id: 'Поставщик',
+        header: 'Поставщик',
+        cell: ({ row }) => row.original.supplier?.name,
+    },
+    {
+        id: 'Способ оплаты',
+        accessorFn: (row) =>
+            row.supplierInvoice?.paymentOption &&
+            paymentOptions[row.supplierInvoice?.paymentOption],
+    },
+    {
+        id: 'Сумма товаров',
+        header: () => <div className='text-right'>Сумма товаров</div>,
+        cell: ({ row }) => {
+            const totalCost = row.original.supplierInvoice?.accountsPayable
+                ? parseFloat(row.original.supplierInvoice?.accountsPayable)
+                : 0
+
+            return (
+                <div className='w-full text-right'>
+                    <CurrencyFormatter value={totalCost} />
+                </div>
+            )
+        },
+    },
+    {
+        id: 'Сумма оплаты',
+        header: () => <div className='text-right'>Сумма оплаты</div>,
+        cell: ({ row }) => {
+            const amountPaid = row.original.supplierInvoice?.amountPaid
+                ? parseFloat(row.original.supplierInvoice?.amountPaid)
+                : 0
+
+            return (
+                <div className='w-full text-right'>
+                    <CurrencyFormatter value={amountPaid} />
+                </div>
+            )
+        },
+    },
+    {
+        id: 'Долг поставщику',
+        header: () => <div className='text-right'>Долг поставщику</div>,
+        cell: ({ row }) => {
+            const amountPaid = row.original.supplierInvoice?.amountPaid
+                ? parseFloat(row.original.supplierInvoice?.amountPaid)
+                : 0
+
+            const totalCost = row.original.supplierInvoice?.accountsPayable
+                ? parseFloat(row.original.supplierInvoice?.accountsPayable)
+                : 0
+
+            return (
+                <div className='w-full text-right'>
+                    <CurrencyFormatter value={totalCost - amountPaid} />
+                </div>
+            )
+        },
+    },
+    {
+        id: 'actions',
+        cell: ({ row }) => {
+            const { id, isArchived } = row.original
+
+            return <GoodsReceiptActions id={id} isArchived={isArchived} />
+        },
+        enableSorting: false,
+        enableHiding: false,
+    },
 ]
